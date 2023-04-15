f81ea3700070d261b75e55eb11d23d2d9fe2ad82
Tatsuya Sakamoto <tatsuya.sakamoto@jmty.jp>
2023-04-15 20:08:42 +0900

    protobufの定義

---
+++ app/src/main/java/com/example/proto_data_store_sample/data/UserPreferencesSerializer.kt
@@ -0,0 +1,21 @@
+package com.example.proto_data_store_sample.data
+
+import androidx.datastore.core.CorruptionException
+import androidx.datastore.core.Serializer
+import com.example.proto_data_store_sample.datastore.UserPreferences
+import com.google.protobuf.InvalidProtocolBufferException
+import java.io.InputStream
+import java.io.OutputStream
+
+object UserPreferencesSerializer : Serializer<UserPreferences> {
+    override val defaultValue: UserPreferences = UserPreferences.getDefaultInstance()
+    override suspend fun readFrom(input: InputStream): UserPreferences {
+        try {
+            return UserPreferences.parseFrom(input)
+        } catch (exception: InvalidProtocolBufferException) {
+            throw CorruptionException("Cannot read proto.", exception)
+        }
+    }
+
+    override suspend fun writeTo(t: UserPreferences, output: OutputStream) = t.writeTo(output)
+}
