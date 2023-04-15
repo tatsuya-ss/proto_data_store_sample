841b27fa8a4effeb017927dc698f2120ef7e4a70
Tatsuya Sakamoto <tatsuya.sakamoto@jmty.jp>
2023-04-15 19:56:12 +0900

    プロジェクト作成

---
+++ app/src/main/java/com/example/proto_data_store_sample/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.proto_data_store_sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
